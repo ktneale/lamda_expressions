4e8fac7973542739ad708f387304bfe66b09b47c
ktneale <kt.neale@gmail.com>
2015-09-25 12:09:42 +0100

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,30 @@
 #include <iostream>
 #include <vector>
-#include <algorithm> 
+#include <algorithm>
 
 int main()
 {
     using namespace std;
 
 
-    std::vector<int> c {1,2,3,4,5,6,7,8,9};
+    std::vector < int >c {
+    1, 2, 3, 4, 5, 6, 7, 8, 9};
 
     int multiple = 5;
-    size_t cnt = count_if(c.begin(), c.end(), [=](int i){ return !(i % multiple); });
-    std::cout << "Number of multiples of " << multiple << " is: " << cnt << std::endl;
+    size_t cnt =
+	count_if(c.begin(), c.end(),[ =] (int i) { return !(i % multiple);
+		 }
+    );
+    std::
+	cout << "Number of multiples of " << multiple << " is: " << cnt <<
+	std::endl;
 
     int sum = 0;
 
-    cnt = count_if(c.begin(), c.end(), [&](int i){ return sum+=i; });
+    cnt = count_if(c.begin(), c.end(),[&](int i) {
+		   return sum += i;
+		   }
+    );
 
     std::cout << "Sum is: " << sum << std::endl;
     return 0;
